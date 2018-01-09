31bb541f2215f0d1a471f000a7f315a632f60846
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:55:51 -0800

    Can show restaurants via main menu.

--- ratings.py
+++ ratings.py
@@ -41,13 +41,40 @@ def show_sorted_ratings():
     """Prints an alphabetized display of restaurant ratings."""
 
     restaurant_scores = clean_up_data(sys.argv[1])
-    users_restaurant, users_score = add_users_rating()
+    # users_restaurant, users_score = add_users_rating()
 
-    restaurant_scores[users_restaurant] = users_score
+    # restaurant_scores[users_restaurant] = users_score
 
     sorted_rests = sorted(restaurant_scores.items())
 
     for rest_name, rest_score in sorted_rests:
         print "{} has a rating of {}.".format(rest_name, rest_score)
 
-show_sorted_ratings()
+# show_sorted_ratings()
+
+
+def main_menu():
+    """Displays options to user."""
+
+    print """
+                (S)ee all restaurant ratings
+                (A)dd a new restaurant rating
+                (Q)uit
+    """
+
+    user_choice = raw_input("What would you like to do? > ")
+
+    if user_choice == "S":
+        show_sorted_ratings()
+
+        # pass
+        # function to show all ratings sorted
+
+    elif user_choice == "A":
+        pass
+        # function to add a restaurant
+
+    elif user_choice == "Q":
+        pass
+    else:
+        print "Sorry, I didn't understand your choice!"
