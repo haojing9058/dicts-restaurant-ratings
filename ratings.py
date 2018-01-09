97d796cdcfe9bdfbc99ee27ec3dce0a5e0a14493
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:20:46 -0800

    Rudimentary ability to accept user input.

--- ratings.py
+++ ratings.py
@@ -16,20 +16,36 @@ def clean_up_data(filename):
     return restaurant_scores
 
 
+def add_users_rating():
+    """"""
+
+    users_scores = []
+
+    users_restaurant = raw_input("Where have you eaten recently? > ")
+    users_restaurant = users_restaurant.title()
+
+    users_rating = raw_input("Cool! How would you rate it out of 5? > ")
+    users_rating = int(users_rating)
+
+    users_scores.extend([users_restaurant, users_rating])
+
+    return users_scores
+
+
 def show_sorted_ratings():
     """Parses a text file and returns
     an alphabetized list of restaurant ratings."""
 
     restaurant_scores = clean_up_data(sys.argv[1])
+    users_scores = add_users_rating()
+    users_restaurant = users_scores[0]
+
+    if users_restaurant not in restaurant_scores:
+        restaurant_scores[users_restaurant] = users_scores[1]
 
     sorted_rests = sorted(restaurant_scores.items())
 
     for rest_rating in sorted_rests:
         print "{} has a rating of {}.".format(rest_rating[0], rest_rating[1])
 
-
 show_sorted_ratings()
-
-# new function taking user input (twice)
-    # takes restaurant name
-    # takes restaurant score
