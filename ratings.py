6ae100f6b73b25b2c4f43a0fe89e3c0d8989eb3d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-08 17:38:36 -0800

    Refactored code.

--- ratings.py
+++ ratings.py
@@ -6,46 +6,40 @@ def clean_up_data(filename):
 
     restaurant_scores = {}
 
-    list_of_ratings = open(filename)
-    for line in list_of_ratings:
-        line = line.rstrip()
-        rest_ratings = line.split(":")
+    # list_of_ratings = open(filename)
+    with open(filename) as list_of_ratings:
+        for line in list_of_ratings:
+            line = line.rstrip()
+            rest_name, rest_score = line.split(":")
 
-        restaurant_scores[rest_ratings[0]] = rest_ratings[1]
+            restaurant_scores[rest_name] = rest_score
 
     return restaurant_scores
 
 
 def add_users_rating():
-    """"""
-
-    users_scores = []
+    """Asks for user's restaurant and rating, returns info in a list."""
 
     users_restaurant = raw_input("Where have you eaten recently? > ")
     users_restaurant = users_restaurant.title()
 
-    users_rating = raw_input("Cool! How would you rate it out of 5? > ")
-    users_rating = int(users_rating)
+    users_score = raw_input("Cool! How would you rate it out of 5? > ")
+    users_score = int(users_score)
 
-    users_scores.extend([users_restaurant, users_rating])
-
-    return users_scores
+    return [users_restaurant, users_score]
 
 
 def show_sorted_ratings():
-    """Parses a text file and returns
-    an alphabetized list of restaurant ratings."""
+    """Prints an alphabetized display of restaurant ratings."""
 
     restaurant_scores = clean_up_data(sys.argv[1])
-    users_scores = add_users_rating()
-    users_restaurant = users_scores[0]
+    users_restaurant, users_score = add_users_rating()
 
-    if users_restaurant not in restaurant_scores:
-        restaurant_scores[users_restaurant] = users_scores[1]
+    restaurant_scores[users_restaurant] = users_score
 
     sorted_rests = sorted(restaurant_scores.items())
 
-    for rest_rating in sorted_rests:
-        print "{} has a rating of {}.".format(rest_rating[0], rest_rating[1])
+    for rest_name, rest_score in sorted_rests:
+        print "{} has a rating of {}.".format(rest_name, rest_score)
 
 show_sorted_ratings()
